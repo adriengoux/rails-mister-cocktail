669384a4bb4065a2169175a8b2f15c6efe63db8d
adrien goux <adriengoux@gmail.com>
2020-10-27 20:47:44 +0800

    add picture to cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   # get 'cocktails/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:index, :new, :create, :destroy]
     # resources :ingredients, only: [ :new, :create ]
